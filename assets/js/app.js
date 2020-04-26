c97764effb0e4c511875ce054ba6253fd39828a5
rdoyle99 <rpdoyle1@gmail.com>
2020-04-26 11:32:09 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+//JQuery Module Pattern
+// An object literal
+var app = {
+  init: function() {
+    app.functionOne();
+  },
+  functionOne: function () {
+  }
+};
+$("document").ready(function () {
+  app.init();
+  
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy"
+    // ... more custom settings?
+  });
+
+  // Start Mobile Nav Function
+  $(".hamburger-menu, .close-menu").click(function(){
+    $(".mobile-nav").toggle();
+  });
+  // End Mobile Name
+
+  // Start Sticky Header on Scroll Up
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+
+    if(st < 5){
+      $("nav.nav-section, body").removeClass("fixed")
+    }else{
+      if (st < lastScrollTop){
+          $("nav.nav-section, body").addClass("fixed")
+      }else{
+          $("nav.nav-section, body").removeClass("fixed")
+      }
+    }
+
+    lastScrollTop = st;
+  });
+  
+  // End Sticky Header on Scroll Up
+
+});
